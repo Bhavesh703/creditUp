09d2f91b6395959fc3b8bd79d5a716773b0db100
Bhavesh <guptabhavesh303@gmail.com>
2024-02-19 23:41:20 +0530

    init update

--- js/index.js
+++ js/index.js
@@ -33,3 +33,14 @@ window.addEventListener("scroll", function () {
     scrolldiv.style.display = "none";
   }
 });
+
+//loader
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
